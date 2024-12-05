4ea3d5ca6b64261c112de4bd57813484ff5f082b
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-05 11:36:47 -0300

    confgi: add styles e assets

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,11 @@
 import "@/styles/global.css"
 import { Slot } from "expo-router"
+import { StatusBar } from "expo-status-bar"
 
 export default function RootLayout() {
     return (
         <>
+         <StatusBar style="light" backgroundColor="transparent" translucent />
             <Slot />
         </>
     )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
     return (
-        <View>
-            <Text>Teams App</Text>
+        <View className="flex-1 bg-[#09090b] items-center justify-center">
+            <Text className="text-[#fafafa] text-2xl font-bold">Groups</Text>
         </View>
     )
 }
