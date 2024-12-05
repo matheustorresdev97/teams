c16dab6d8beb634f86514f555701a2cebdbe8322
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-05 14:33:01 -0300

    feat: get all groups

--- src/storage/group/group-create.ts
+++ src/storage/group/group-create.ts
@@ -1,9 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { GROUP_COLLECTION } from '../storage-config'
+import { groupGetAll } from './group-get-all'
 
 export async function groupCreate(newGroup: string) {
     try {
-        await AsyncStorage.setItem(GROUP_COLLECTION, newGroup)
+        const storedGroups = await groupGetAll()
+
+        const storage = JSON.stringify([...storedGroups, newGroup])
+
+        await AsyncStorage.setItem(GROUP_COLLECTION, storage)
     } catch (error) {
         throw error
     }
