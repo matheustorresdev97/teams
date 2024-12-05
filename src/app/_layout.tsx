b4ee7c634a87f68a01094aa4f83072ea8314c0ae
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-05 11:38:26 -0300

    config: add fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,11 +1,26 @@
 import "@/styles/global.css"
 import { Slot } from "expo-router"
 import { StatusBar } from "expo-status-bar"
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 
 export default function RootLayout() {
+    const [fontsLoaded] = useFonts({
+        Roboto_400Regular,
+        Roboto_700Bold,
+    })
+
+    if (!fontsLoaded) {
+        return <ActivityIndicator />
+    }
+
     return (
         <>
-         <StatusBar style="light" backgroundColor="transparent" translucent />
+            <StatusBar style="light" backgroundColor="transparent" translucent />
             <Slot />
         </>
     )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 
 export default function Index() {
     return (
-        <View className="flex-1 bg-[#09090b] items-center justify-center">
+        <View className="flex-1 bg-gray-600 items-center justify-center">
             <Text className="text-[#fafafa] text-2xl font-bold">Groups</Text>
         </View>
     )
